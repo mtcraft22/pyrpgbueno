c93ca06ce8463d194ef9b6fdffddb881f892e8d0
mtcraft22 <97896526+mtcraft22@users.noreply.github.com>
2022-04-06 10:57:14 +0200

    Update classes_entidades.py

--- new_mod_pyrpg/classes_entidades.py
+++ new_mod_pyrpg/classes_entidades.py
@@ -25,3 +25,38 @@ class Enemigo(Entidades):
 
 
 
+class Entidades():
+    def __init__(self,nombre,ataque,defensa,vida):
+        self.nombre=nombre
+        self.ataque=ataque
+        self.defensa=defensa
+        self.vida_max=vida
+        self.vida_act=vida
+        self.habilidades=[]
+        self.estado="Normal"
+    def atacar(self, destino):
+        destino.vida_act -= (self.ataque + destino.defensa)
+    def cambio_estado(self,destino,estado):
+        destino.estado=estado
+class Jugador(Entidades):
+    def __init__(self,escudo,nombre,ataque,defensa,vida):
+        super().__init__(nombre,ataque,defensa,vida)
+        self.escudo_max=escudo
+        self.escudo_act=escudo
+class Enemigos(Entidades):
+    def __init__(self,planta,peso,nombre,ataque,defensa,vida):
+        super().__init__(nombre,ataque,defensa,vida)
+        self.planta_minima=planta
+        self.peso=peso
+class Enemgos_complejos(Enemigos):
+    def __init__(self,planta,peso,transformaciones,nombre,ataque,defensa,vida):
+        super().__init__(nombre,planta,peso,ataque,defensa,vida)
+        self.transformaciones=transformaciones
+
+
+J=Enemigos(12,"32",12,12,12,12)
+
+if type(J)==Jugador:
+    print("es un jugador")
+
+
